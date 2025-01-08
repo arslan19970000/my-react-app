85a2ae434b69ee5026d307e00a498c8b54b06cf8
arslan19970000 <167683066+arslan19970000@users.noreply.github.com>
2025-01-08 14:20:57 +0500

    Create Contact.js
    this is the  new file that i forget

---
+++ Contact.js
@@ -0,0 +1,14 @@
+
+// src/Contact.js
+import React from 'react';
+
+const Contact = () => {
+  return (
+    <div>
+      <h1>Contact Us</h1>
+      <p>Email: contact@example.com</p>
+    </div>
+  );
+};
+
+export default Contact;
